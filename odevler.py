35c5d61258f981fa2482fd4ba7d0e35fad246eed
turankarbuga <43503771+turankarbuga@users.noreply.github.com>
2018-09-22 22:46:55 +0300

    Add files via upload

---
+++ odevler.py
@@ -0,0 +1,86 @@
+
+#problem1
+
+"""
+sayı1=int(input("1. sayı:"))
+sayı2=int(input("2.sayı:"))
+sayı3=int(input("3.sayı:"))
+çarpım= sayı1*sayı2*sayı3
+print("sayıların çarpımı {}".format(çarpım))
+"""
+
+#problem2
+
+"""
+boy = float(input("Boy (m):"))
+kilo = int(input("Kilo:"))
+indeks = kilo / boy **2
+print(indeks)
+"""
+
+#problem3
+
+"""
+araç_yakış= float(input("Kilometre başına harcanan yakıt (L):"))
+alınan_yol= float(input("Kaç kilometre yol gidildi?:"))
+benzin_fiyat=float(input("Benzin Litre fiyatı:"))
+para =  araç_yakış*alınan_yol*benzin_fiyat
+print("masrafınız:",para)
+"""
+
+#problem4
+
+"""
+ad=input("ad:")
+soyad= input("soyad:")
+numara=input("telefon numarası:")
+print("Ad:{}\nSoyad:{}\nNumara:{}".format(ad,soyad,numara))
+"""
+
+#problem5
+
+"""
+sayı1=int(input("ilk sayıyı giriniz:"))
+sayı2=int(input("ikinci sayıyı giriniz:"))
+
+print("sayı1:",sayı1,"\n"+"sayı2:",sayı2,sep="")
+print("sayılar değiştiriliyor...")
+
+sayı1,sayı2=sayı2,sayı1
+
+print("sayı1:",sayı1,"\n"+"sayı2:",sayı2,sep="10")
+"""
+
+#problem6
+
+""""
+a=int(input("dik kenar uzunluğu:"))
+b=int(input("dik kenar uzunluğu:"))
+c=a**2+b**2
+print(c**0.5)
+"""
+
+#ortalama
+
+""""
+a= int(input("Matematik Ders Saati:"))
+b= int(input("Fen Ders Saati:"))
+c= int(input("Türkçe Ders Saati:"))
+d= int(input("İnkılap Ders Saati:"))
+e= int(input("İngilizce Ders Saati:"))
+f= int(input("Din Kültürü Ders Saati:"))
+g= int(input("Beden Ders Saati:"))
+
+an= int(input("Matematik Puanı:"))
+bn= int(input("Fen Puanı:"))
+cn= int(input("Türkçe Puanı:"))
+dn= int(input("İnkılap Puanı:"))
+en= int(input("İngilizce Puanı:"))
+fn= int(input("Din Kültürü Puanı:"))
+gn= int(input("Beden Ders Puanı:"))
+
+Ortalama=((a*an)+(b*bn)+(c*cn)+(d*dn)+(e*en)+(f*fn)+(g*gn))/ (a+b+c+d+e+f+g)
+
+print (Ortalama)
+"""
+
