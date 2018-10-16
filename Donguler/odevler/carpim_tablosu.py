3a9e3e7b6d7217b055745d6e3a9d1955c394ffef
turankarbuga <43503771+turankarbuga@users.noreply.github.com>
2018-10-16 22:47:21 +0300

    Add files via upload

---
+++ Donguler/odevler/carpim_tablosu.py
@@ -0,0 +1,10 @@
+print('''
+********************
+Çarpım Tablosu v1.0
+********************
+''')
+
+for i in range(1,11):
+    print("~.~.~.~.~.~.~.~.~")
+    for j in range(1,11):
+        print("{} * {} = {}".format(i,j,i*j))
