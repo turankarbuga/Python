f7f21cc303ad3d19653cd2fb9a628763e082bc35
turankarbuga <43503771+turankarbuga@users.noreply.github.com>
2019-10-21 23:57:42 +0300

    Update window_class.py

--- PyQt5/window_class.py
+++ PyQt5/window_class.py
@@ -1,6 +1,5 @@
 import sys
-from PyQt5 import QtWidgets
-
+from PyQt5 import QtWidgets,QtGui
 
 class Window(QtWidgets.QWidget):
     def __init__(self):
@@ -10,38 +9,46 @@ class Window(QtWidgets.QWidget):
 
     def init_ui(self):
 
-        self.label1 = QtWidgets.QLabel("Bana Henüz Tıklanmadı.")
-        self.button1 = QtWidgets.QPushButton("Tıkla")
-        self.button2 = QtWidgets.QPushButton("Sıfırla")
+        self.label1 = QtWidgets.QLabel("Haven't clicked yet.")
+        self.label1.setFont(QtGui.QFont("Times",24))
+        self.label2 = QtWidgets.QLabel("0")
+        self.label2.setFont(QtGui.QFont("Arial",50))
+        self.click_button = QtWidgets.QPushButton("Click")
+        self.clear_button = QtWidgets.QPushButton("Clear")
         self.count = 0
 
         self.setGeometry(300,300,600,500)
 
         v_box = QtWidgets.QVBoxLayout()
         v_box.addWidget(self.label1)
-        v_box.addWidget(self.button1)
-        v_box.addWidget(self.button2)
-        v_box.addStretch()
+        v_box.addWidget(self.click_button)
+        v_box.addWidget(self.clear_button)
 
         h_box = QtWidgets.QHBoxLayout()
-
         h_box.addStretch()
         h_box.addLayout(v_box)
         h_box.addStretch()
 
-        def increase():
+        def click():
             self.count += 1
-            self.label1.setText("Bana {} kere tıklandı".format(self.count))
-        def clear():
-            self.count = 0
-            self.label1.setText("Bana Henüz Tıklanmadı.")
+            if self.count == 1:
+                self.label1.setText("Clicked 1 time.")
+                self.label2.setText("{}".format(self.count))
+            else:
+                self.label1.setText("Clicked {} times.".format(self.count))
+                self.label2.setText("{}".format(self.count))
 
-        self.button1.clicked.connect(increase)
-        self.button2.clicked.connect(clear)
+        def clear():
+            self.label1.setText("Haven't clicked yet.")
+            self.count = 0
+
+        self.click_button.clicked.connect(click)
+        self.clear_button.clicked.connect(clear)
 
         self.setLayout(h_box)
 
         self.show()
+
 
 
 app = QtWidgets.QApplication(sys.argv)
