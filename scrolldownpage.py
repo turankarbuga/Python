0b8dadc5be26719c749e4cff048a404c680345c9
turankarbuga <43503771+turankarbuga@users.noreply.github.com>
2019-12-04 15:24:57 +0300

    Create scrolldownpage.py

---
+++ scrolldownpage.py
@@ -0,0 +1,10 @@
+# js scroll down page
+
+lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+match=False
+while(match==False):
+    lastCount = lenOfPage
+    time.sleep(1)
+    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    if lastCount == lenOfPage:
+        match=True
