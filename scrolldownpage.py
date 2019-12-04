f916cda9bf1b2ebaad01c2216afe7694f8defcf6
turankarbuga <43503771+turankarbuga@users.noreply.github.com>
2019-12-04 21:54:00 +0300

    Update scrolldownpage.py

--- scrolldownpage.py
+++ scrolldownpage.py
@@ -8,3 +8,22 @@ while(match==False):
     lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
     if lastCount == lenOfPage:
         match=True
+
+        
+# Sample use
+
+jscommand = """
+followers_list = document.querySelector("div[role = 'dialog'] ul") # select div
+followers_list.scrollTo(0, follows_list.scrollHeight); 
+var lenOfPage=followers_list.scrollHeight;
+return lenOfPage;
+"""
+
+lenOfPage = self.browser.execute_script(jscommand)
+match = False
+while (match == False):
+    lastCount = lenOfPage
+    time.sleep(1)
+    lenOfPage = self.browser.execute_script(jscommand)
+    if lastCount == lenOfPage:
+        match = True
